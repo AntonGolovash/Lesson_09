8872c70bc5677b75e9427eebb1d4c418658c3328
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-02-05 16:43:38 +0200

    2

--- Lesson_09/Lesson_09.cpp
+++ Lesson_09/Lesson_09.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
+#include "Car.h"
+#include "Car.cpp"
+#include "Audi.h"
+#include "Audi.cpp"
+#include "BMW.h"
+#include "BMW.cpp"
+using namespace std;
+
+template<class T>
+void PolimorphDisplay(Car<T>* car)
+{
+	cout << car->ToString() << endl;
+}
 
 int main()
 {
+	Audi<int> audi;
+	BMW<int> bmw;
+	Car<int>* car;
+
+	car = &audi;
+	car->SetParams(3, "{\"model\": \"100\", \"color\" : \"red\", \"capacity\" : \"2.3\"}");
+	car->SetParams(6, "{\"model\": \"100\", \"color\" : \"red\", \"capacity\" : \"2.3\"}");
+	cout << "###Audi 100###" << endl;
+	PolimorphDisplay(car);
+
+	car = &bmw;
+	car->SetParams(3, "{\"model\": \"520\", \"color\" : \"black\", \"capacity\" : \"2.5\"}");
+	car->SetParams(6, "{\"model\": \"520\", \"color\" : \"black\", \"capacity\" : \"2.5\"}");
+	cout << "###BMW 520###" << endl;
+	PolimorphDisplay(car);
 
 }
